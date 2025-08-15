c57cb6b33ec1c684ddec54e5b89b13d7858fb2e8
Dawn McKnight <demcknig@ualberta.ca>
2025-08-15 00:43:14 -0600

    refactor(__init__.py): exposing instantiated kp_accessor rather than class KpAccessor at module level
    BREAKING CHANGE:

--- kp_accessor/__init__.py
+++ kp_accessor/__init__.py
@@ -1,8 +1,8 @@
-from kp_accessor.kp_accessor import KpAccessor
+from kp_accessor.kp_accessor import kp_accessor
 from kp_accessor.data_retrieval import CACHE_PATH, TABLE_PATH, URL
 
 __all__ = [
-    "KpAccessor",
+    "kp_accessor",
     "CACHE_PATH",
     "TABLE_PATH",
     "URL",
